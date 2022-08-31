7760c10d55288b3e3a3cc316886a0c167341b6bb
Hector Candelaria <hcandelaria66.7@gmail.com>
2022-08-30 23:21:33 -0400

    Integrated context api.

--- catch-em-all/ClientApp/src/App.js
+++ catch-em-all/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
+import { PokemonProvider } from './context/PokemonContext';
 import './custom.css';
 
 export default class App extends Component {
@@ -9,14 +10,16 @@ export default class App extends Component {
 
   render() {
     return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
+      <PokemonProvider>
+        <Layout>
+          <Routes>
+            {AppRoutes.map((route, index) => {
+              const { element, ...rest } = route;
+              return <Route key={index} {...rest} element={element} />;
+            })}
+          </Routes>
+        </Layout>
+      </PokemonProvider>
     );
   }
 }
